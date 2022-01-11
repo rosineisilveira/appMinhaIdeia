0e4e6db045183051cd6c9affa510b6dde45feccc
rosineisilveira <rosinei.s@hotmail.com>
2022-01-10 21:12:57 -0300

    Initial commit

---
+++ appminhaideia/src/main/java/rosineiSilveira/appminhaideia/SplashActivity2.kt
@@ -0,0 +1,30 @@
+package rosineiSilveira.appminhaideia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity2 : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+
+         handler = Handler()
+         handler.postDelayed({
+
+
+             val intent = Intent(this, MainActivity::class.java);
+             startActivity(intent);
+             finish()
+         },3000)
+
+    }
+
+}
+
+
+
+
